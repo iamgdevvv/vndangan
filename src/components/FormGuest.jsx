949345a2464c848a0f38484c7d3937db86217618
Grafis Whello <grafis@whello.id>
2022-11-28 23:56:04 +0700

    fix build error in dummy form guest

--- src/components/FormGuest.jsx
+++ src/components/FormGuest.jsx
@@ -1,7 +1,6 @@
 export default function FormGuest() {
 	return (
 		<form action='https://submit-form.com/zq8ntdSi'>
-			<label for='name'>Name</label>
 			<input
 				type='text'
 				id='name'
@@ -9,20 +8,12 @@ export default function FormGuest() {
 				placeholder='Name'
 				required=''
 			/>
-			<label for='email'>Email</label>
-			<input
-				type='email'
-				id='email'
-				name='email'
-				placeholder='Email'
-				required=''
-			/>
-			<label for='message'>Message</label>
 			<textarea
 				id='message'
 				name='message'
 				placeholder='Message'
-				required=''></textarea>
+				required=''
+			/>
 			<button type='submit'>Send</button>
 		</form>
 	);
